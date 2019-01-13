e5b2e3f36b3f90396635a3298cc917c383e115f4
Theomat <theomatricon@gmail.com>
2019-01-13 11:48:20 +0100

    + Added documentation + Support for ethernet frames of any payload size + Added destroy to free used resources

--- src/net_interface.c
+++ src/net_interface.c
@@ -19,37 +19,47 @@ struct net_interface {
   uint32_t ip;
   unsigned char mac[6];
 };
-
+//------------------------------------------------------------------------------
+//                               GETTERS
+//------------------------------------------------------------------------------
 uint32_t net_interface_get_ip(struct net_interface* interface) {
   assert(interface != NULL);
   return interface->ip;
 }
-
 unsigned char* net_interface_get_mac(struct net_interface* interface) {
   assert(interface != NULL);
   return interface->mac;
 }
 
-static struct eth_header* header_buffer = NULL;
-
+static struct eth_header* eth_header = NULL;
+static uint32_t max_payload_size     = 0;
+//------------------------------------------------------------------------------
+//                           SEND DATA
+//------------------------------------------------------------------------------
 void net_interface_send(struct net_interface* interface, char* content,
                         uint32_t len, unsigned char* dst_mac) {
   assert(interface != NULL);
-  assert(header_buffer != NULL);
-  ethernet_header_set(header_buffer, ETH_P_ARP, content, len, interface->mac,
-                      dst_mac);
-  printf("[DEBUG][SEND] Payload size=%u ", len);
-  ethernet_fprint(header_buffer, stdout);
-  printf("\n");
-
-  tun_write((char*)header_buffer, len + ETHERNET_HEADER_SIZE + 1);
-}
-
-struct net_interface* net_interface_alloc(char* ip, char* mac) {
-  if (header_buffer == NULL) {
-    header_buffer = ethernet_header_alloc();
+  // Ensure we have enough size of payload in our ethernet header
+  if (len > max_payload_size || max_payload_size == 0) {
+    if (eth_header != NULL)
+      free(eth_header);
+    eth_header       = ethernet_header_alloc(len);
+    max_payload_size = len;
   }
+  ethernet_header_set(eth_header, ETH_P_ARP, content, len, interface->mac,
+                      dst_mac);
+#if DEBUG
+  printf("[DEBUG][SEND] Payload size=%u ", len);
+  ethernet_fprint(eth_header, stdout);
+  printf("\n");
+#endif
 
+  tun_write((char*)eth_header, len + ETHERNET_HEADER_SIZE);
+}
+//------------------------------------------------------------------------------
+//                           ALLOCATION
+//------------------------------------------------------------------------------
+struct net_interface* net_interface_alloc(char* ip, char* mac) {
   struct net_interface* interface = calloc(1, sizeof(struct net_interface));
   memcpy(interface->mac, mac, 6);
   if (inet_pton(AF_INET, ip, &interface->ip) != 1) {
@@ -57,4 +67,11 @@ struct net_interface* net_interface_alloc(char* ip, char* mac) {
     exit(1);
   }
   return interface;
+}
+//------------------------------------------------------------------------------
+//                           LIFECYCLE
+//------------------------------------------------------------------------------
+void net_interface_destroy() {
+  if (eth_header != NULL)
+    free(eth_header);
 }
--- src/net_interface.h
+++ src/net_interface.h
@@ -1,10 +1,23 @@
 #ifndef NET_INTERFACE_H
 #define NET_INTERFACE_H
 struct net_interface;
+//------------------------------------------------------------------------------
+//                               GETTERS
+//------------------------------------------------------------------------------
 uint32_t net_interface_get_ip(struct net_interface* interface);
 unsigned char* net_interface_get_mac(struct net_interface* interface);
+//------------------------------------------------------------------------------
+//                           SEND DATA
+//------------------------------------------------------------------------------
 void net_interface_send(struct net_interface* interface, char* content,
                         uint32_t len, unsigned char* dst_mac);
+//------------------------------------------------------------------------------
+//                           ALLOCATION
+//------------------------------------------------------------------------------
 struct net_interface* net_interface_alloc(char* ip, char* mac);
+//------------------------------------------------------------------------------
+//                           LIFECYCLE
+//------------------------------------------------------------------------------
+void net_interface_destroy();
 
 #endif
